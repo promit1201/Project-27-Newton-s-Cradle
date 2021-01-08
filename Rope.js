f40d86300d12789eed6100425afb84d392579315
promit1201 <73183721+promit1201@users.noreply.github.com>
2021-01-08 13:23:27 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,38 @@
+class Rope
+{
+  constructor(body1, body2, offsetX, offestY)
+  {
+
+     this.offsetX=offestX;
+     this.offsetY=offsetY;
+      var options {
+        bodyA:body1,
+        bodyB:body2,
+        pointA:{x:this.offsetX, y:this.offsetY}
+      }
+     
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
